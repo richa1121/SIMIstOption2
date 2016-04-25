f8ee3912637e360841ea8f07968d11d8d93a33aa
sns5408 <sns5408@208-NWD11.up.ist.local>
2016-04-25 14:49:52 -0400

    test

--- SIMIstOption2/src/simistoption2/SIMIstOption2.java
+++ SIMIstOption2/src/simistoption2/SIMIstOption2.java
@@ -15,6 +15,8 @@ public class SIMIstOption2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //suzanne
+       
         // TODO code application logic here
     }
     
