229ca7ac9d218f54f5e821100246cbb06e5bf8f7
Suzariah <Suz2478@gmail.com>
2016-04-29 15:30:17 -0400

    added moving player for room208

---
+++ SIMIstOption2/src/simistoption2/Room208.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simistoption2;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author sns5408
+ */
+public class Room208 extends JPanel implements ActionListener, KeyListener{
+    
+    private Image floorImage;
+   private Timer Room208Timer;
+   private player player1;
+   
+   public Room208(){
+       super();
+       Room208Timer = new Timer (50, this);
+       Room208Timer.start();
+       
+       
+        player1 = new player();
+        
+        
+         this.addKeyListener(this);
+         requestFocusInWindow();
+       this.setFocusable(true);
+        
+        
+       
+   }
+     public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+         g.clearRect(0, 0, this.getWidth(), this.getHeight());
+        ImageIcon Icon = new ImageIcon("floor tiles.jpg");
+        g.drawImage(Icon.getImage(), 0, 0, this);
+        player1.paintComponent(g);
+        
+        
+    }
+    
+    public void actionPerformed(ActionEvent e){
+       Object o = e.getSource();
+       
+       if(o == Room208Timer){
+           this.repaint();
+       }
+   }  
+    public void keyPressed(KeyEvent e){
+       if(e.getKeyCode()== KeyEvent.VK_LEFT){
+        player1.setDx(-20);
+       
+        System.out.println("Typed");
+       }
+       
+       else if(e.getKeyCode() ==KeyEvent.VK_RIGHT){
+           player1.setDx(20);
+       }
+        else if(e.getKeyCode() ==KeyEvent.VK_UP){
+           player1.setDy(-20);
+        }
+            else if(e.getKeyCode() ==KeyEvent.VK_DOWN){
+           player1.setDy(20);
+            }
+    }   
+    
+    public void keyReleased(KeyEvent e){
+        player1.setDx(0);
+        player1.setDy(0);
+    }
+    public void keyTyped(KeyEvent e){
+        
+    }
+}
